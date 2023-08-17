90e7ae11a0d1942531748733a50fa25f311d1b91
Angelo <angelogoves@gmail.com>
2023-08-17 14:59:21 +0530

    Updated Calculator.py file with subtract function

--- calculator.py
+++ calculator.py
@@ -1,8 +1,14 @@
 class Calculator:
     def add(self, num1, num2):
         return num1 + num2
+    
+    def subtract(self, num1, num2):
+        return num1 - num2
 
 calculator = Calculator()
 
 result = calculator.add(1,2)
+print(result)
+
+result = calculator.subtract(1,2)
 print(result)
