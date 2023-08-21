305d0d353942bf79f4a8d9f0bbd231c7b08fd5ae
Angelo <angelogoves@gmail.com>
2023-08-22 00:53:42 +0530

    Completed unit tests for Exercise 4ruby

---
+++ RUBY/exercise-04/exercise-04.rb
@@ -0,0 +1,28 @@
+class Exercise
+  def initialize(sports)
+      @sports = sports
+  end
+  def sports=(sports)
+      @sports = sports
+  end
+  def sports
+      return @sports
+  end
+
+  def skip_sports(num)
+      updatedSports = []
+      sports.each_with_index do |value, index|
+        keyValue = "#{index}:#{value}"
+        updatedSports << keyValue
+      end
+      return updatedSports[num..]
+  end
+end
+
+sports = ["Cricket", "TT", "Football", "Hockey"]
+
+obj = Exercise.new(sports)
+print obj.skip_sports(1)
+print obj.skip_sports(2)
+print obj.skip_sports(3)
+print obj.skip_sports(4)
