a1fdf943c445144d77c2de7a9b27ac45f80d1f50
Angelo <angelogoves@gmail.com>
2023-08-22 12:58:57 +0530

    Updated exercise 4 as per the ruby naming convention

--- RUBY/exercise-04/exercise-04.rb
+++ RUBY/exercise-04/exercise-04.rb
@@ -8,12 +8,12 @@ class Exercise
   def sports
       return @sports
   end
-  def skip_sports(num)
+  def skip_sports(skip_value)
       updated_sports = []
       sports.each_with_index do |value, index|
-        keyValue = "#{index}:#{value}"
-        updated_sports << keyValue
+        key_value = "#{index}:#{value}"
+        updated_sports << key_value
       end
-      return updated_sports[num..]
+      return updated_sports[skip_value..]
   end
 end
--- RUBY/exercise-04/testcase-04.rb
+++ RUBY/exercise-04/testcase-04.rb
@@ -4,15 +4,15 @@ require_relative 'exercise-04'
 class TestSkipSports < Test::Unit::TestCase
     @@sports = ["Cricket", "TT", "Football", "Hockey"]
     def test_make_exercise_object
-        objExercise = Exercise.new(@@sports)
-        assert objExercise.instance_of? Exercise
+        object_exercise = Exercise.new(@@sports)
+        assert object_exercise.instance_of? Exercise
     end
     def test_skip_sports
-        objExercise = Exercise.new(@@sports)
-        actualOutput = [["1:TT", "2:Football", "3:Hockey"], ["2:Football", "3:Hockey"], ["3:Hockey"], []]
-        assert_equal(actualOutput[0], objExercise.skip_sports(1))
-        assert_equal(actualOutput[1], objExercise.skip_sports(2))
-        assert_equal(actualOutput[2], objExercise.skip_sports(3))
-        assert_equal(actualOutput[3], objExercise.skip_sports(4))
+        object_exercise = Exercise.new(@@sports)
+        actual_output = [["1:TT", "2:Football", "3:Hockey"], ["2:Football", "3:Hockey"], ["3:Hockey"], []]
+        assert_equal(actual_output[0], object_exercise.skip_sports(1))
+        assert_equal(actual_output[1], object_exercise.skip_sports(2))
+        assert_equal(actual_output[2], object_exercise.skip_sports(3))
+        assert_equal(actual_output[3], object_exercise.skip_sports(4))
     end
 end
