8642af29f745435e39efd46e7d59cf0643882e74
Angelo <angelogoves@gmail.com>
2023-08-22 12:51:23 +0530

    Updated exercise 1 as per the ruby naming convention

--- RUBY/exercise-01/exercise-01.rb
+++ RUBY/exercise-01/exercise-01.rb
@@ -1,8 +1,8 @@
 class Exercise
     def average(serial_average)
         values = serial_average.split('-')
-        averageValues = format('%.2f', (values[1].to_f + values[2].to_f) / 2)
-        result =  values[0] + "-" + averageValues.to_s
+        average_values = format('%.2f', (values[1].to_f + values[2].to_f) / 2)
+        result =  values[0] + "-" + average_values.to_s
         return result
     end
 end
--- RUBY/exercise-01/testcase-01.rb
+++ RUBY/exercise-01/testcase-01.rb
@@ -3,14 +3,13 @@ require_relative 'exercise-01'
 
 class TestAverage < Test::Unit::TestCase
     def test_make_exercise_object
-        objExercise = Exercise.new()
-        assert objExercise.instance_of? Exercise
+        object_exercise = Exercise.new()
+        assert object_exercise.instance_of? Exercise
     end
     def test_check_average
-        objExercise = Exercise.new()
-        assert_equal("002-15.00", objExercise.average("002-10:00-20:00"))
-        assert_equal("002-50.00", objExercise.average("002-100:00-00"))
-        assert_equal("002-50.00", objExercise.average("002-100:00-one"))
+        object_exercise = Exercise.new()
+        assert_equal("002-15.00", object_exercise.average("002-10:00-20:00"))
+        assert_equal("002-50.00", object_exercise.average("002-100:00-00"))
+        assert_equal("002-50.00", object_exercise.average("002-100:00-one"))
     end
 end
-
