76a964538b410797370cfa21c46177ac11c54114
Angelo <angelogoves@gmail.com>
2023-08-23 00:05:38 +0530

    Updated changes to updateClock function

--- HTML/exercise-02/script.js
+++ HTML/exercise-02/script.js
@@ -3,9 +3,9 @@ const darkModeToggle = document.getElementById("darkModeToggle");
 
 function updateClock() {
 	const now = new Date();
-	const hours = String(now.getHours()).padStart(2, "0");
-	const minutes = String(now.getMinutes()).padStart(2, "0");
-	const seconds = String(now.getSeconds()).padStart(2, "0");
+	const hours = now.getHours().toString().padStart(2, "0");
+	const minutes = now.getMinutes().toString().padStart(2, "0");
+	const seconds = now.getSeconds().toString().padStart(2, "0");
 	clockElement.textContent = `${hours}:${minutes}:${seconds}`;
 }
 
