b8708c42a724076c44683835429a5d6487258007
Angelo <angelogoves@gmail.com>
2023-08-22 14:17:24 +0530

    Applied suggested changes

--- HTML/exercise-04/script.js
+++ HTML/exercise-04/script.js
@@ -1,20 +1,26 @@
 const user = {
-  name: "Piyush Sharma",
-  designation: "Senior Software Engineer",
-  company: "Infuse Consulting",
-  hobbies: ["Reading", "Listening to music", "Collecting stamps"],
+	name: "Piyush Sharma",
+	designation: "Senior Software Engineer",
+	company: "Infuse Consulting",
+	hobbies: ["Reading", "Listening to music", "Collecting stamps"],
 };
 
-/**
- * Use destructuting to log the following
- */
-const { name, designation, company, hobbies } = user;
+/*** Use destructuting to log the following*/
 
-const printUserProfile = () => {
-  // Piyush Sharma is a Senior Software Engineer at Infuse Consulting. He likes Reading, Listening to music and Collecting stamps
-  console.log(
-    `${name} is a ${designation} at ${company}. He likes ${hobbies[0]}, ${hobbies[1]} and ${hobbies[2]}`
-  );
+const printUserProfile = (user) => {
+	// Piyush Sharma is a Senior Software Engineer at Infuse Consulting. He likes Reading, Listening to music and Collecting stamps
+	const { name, designation, company, hobbies } = user;
+	let add_hobbies = "";
+	for (let each = 0; each < hobbies.length; each++) {
+		if (each == hobbies.length - 1) {
+			add_hobbies += "and " + hobbies[each];
+		} else {
+			add_hobbies += hobbies[each] + ", ";
+		}
+	}
+	console.log(
+		`${name} is a ${designation} at ${company}. He likes ${add_hobbies}`
+	);
 };
 
-printUserProfile();
+printUserProfile(user);
