2038faae10b0c45eb9ff1fd4edbfd20178ed0dd6
Angelo <angelogoves@gmail.com>
2023-08-23 16:50:37 +0530

    used arrow right key to move on slider

--- selenium/exercise-08.rb
+++ selenium/exercise-08.rb
@@ -3,6 +3,7 @@ Selenium::WebDriver::Edge::Service.driver_path = "C:/Users/angel/Desktop/browser
 driver = Selenium::WebDriver.for :edge
 
 wait = Selenium::WebDriver::Wait.new(:timeout => 11)
+driver.manage.window.maximize
 
 driver.get "https://www.globalsqa.com/demo-site/sliders/#Steps"
 
@@ -10,9 +11,12 @@ iframe = driver.find_element(:xpath, '//*[@id="post-2673"]/div[2]/div/div/div[3]
 driver.switch_to.frame iframe
 
 slider = wait.until { driver.find_element(:id, "slider") }
-driver.action.drag_and_drop_by(slider, 11, 0).perform
+span = slider.find_element(:tag_name, "span")
+span.send_keys :arrow_right
+span.send_keys :arrow_right
+span.send_keys :arrow_right
 
 slide_value = driver.find_element(:id, "amount")
 
-puts slide_value.attribute("value")
+puts "Amount " + slide_value.attribute("value")
 driver.quit
