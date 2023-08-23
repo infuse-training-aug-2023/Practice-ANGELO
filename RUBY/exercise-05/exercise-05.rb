44ed4aecd700677e458468cba2e6b8f0aabfc865
Angelo <angelogoves@gmail.com>
2023-08-23 15:35:42 +0530

    Made code readable

--- RUBY/exercise-05/exercise-05.rb
+++ RUBY/exercise-05/exercise-05.rb
@@ -1,23 +1,21 @@
-require 'json'
-require 'time'
+require "json"
+require "time"
 
 class Exercise
-    def json_task
-        json_file = File.read('RUBY/exercise-05/test.json')
-        json_data = JSON.parse(json_file)
-        first_name, last_name = json_data['name'].split(' ')
-        updated_name = Hash.new
-        updated_name.store("First Name", first_name)
-        updated_name.store("Last Name", last_name)
-        json_data.delete("name")
-        updated_data = updated_name.merge(json_data)
-        timestamp = Time.now.utc.to_i
-        output_file_name = "Angelo_#{timestamp}.json"
-        new_file_path = "RUBY/exercise-05/#{output_file_name}"
-        json_data = JSON.generate(updated_data)
-        file_open = File.open(new_file_path,"w")
-        file_open.write(json_data)
-        file_open.close
-        return output_file_name
+  def change_json_data
+    json_file = File.read("RUBY/exercise-05/test.json")
+    json_data = JSON.parse(json_file)
+
+    first_name, last_name = json_data["name"].split(" ")
+    json_data.delete("name")
+    json_data["First Name"] = first_name
+    json_data["Last Name"] = last_name
+
+    timestamp = Time.now.utc.strftime("%Y%m%d%H%M%S")
+    output_file_name = "RUBY/exercise-05/Angelo_#{timestamp}.json"
+    File.open(output_file_name, "w") do |file|
+      file.write(JSON.generate(json_data))
     end
+    return output_file_name
+  end
 end
--- RUBY/exercise-05/testcase-05.rb
+++ RUBY/exercise-05/testcase-05.rb
@@ -1,18 +1,16 @@
-require 'test/unit'
-require_relative 'exercise-05'
+require "test/unit"
+require_relative "exercise-05"
 
 class TestJsonTask < Test::Unit::TestCase
-    def test_make_exercise_object
-        object_exercise = Exercise.new
-        assert object_exercise.instance_of? Exercise
-    end
-    def test_json_task
-        object_exercise = Exercise.new
-        actual_output_json_file = File.read('RUBY/exercise-05/actual_output.json')
-        actual_output = JSON.parse(actual_output_json_file)
-        expected_output_file_name = "RUBY/exercise-05/#{object_exercise.json_task}"
-        expected_output_json_file = File.read(expected_output_file_name)
-        expected_output = JSON.parse(expected_output_json_file)
-        assert_equal(actual_output, expected_output)
-    end
+  def test_make_exercise_object
+    object_exercise = Exercise.new
+    assert object_exercise.instance_of? Exercise
+  end
+
+  def test_change_json_data
+    object_exercise = Exercise.new
+    actual_output = JSON.parse(File.read("RUBY/exercise-05/actual_output.json"))
+    expected_output = JSON.parse(File.read(object_exercise.change_json_data))
+    assert_equal(actual_output, expected_output)
+  end
 end
