32ad16f7ad5e6a7eb3faccba9e5cf1474959e774
Angelo <angelogoves@gmail.com>
2023-08-23 14:32:12 +0530

    Removed unused code

--- RUBY/exercise-04/exercise-04.rb
+++ RUBY/exercise-04/exercise-04.rb
@@ -1,19 +1,17 @@
 class Exercise
   def initialize(sports)
-      @sports = sports
+    @sports = sports
   end
-  def sports=(sports)
-      @sports = sports
-  end
-  def sports
-      return @sports
-  end
+
   def skip_sports(skip_value)
+    if skip_value < @sports.length
       updated_sports = []
-      sports.each_with_index do |value, index|
+      @sports.each_with_index do |value, index|
         key_value = "#{index}:#{value}"
-        updated_sports << key_value
+        updated_sports.append(key_value)
       end
       return updated_sports[skip_value..]
+    end
+    return "Array index is out of bound"
   end
 end
