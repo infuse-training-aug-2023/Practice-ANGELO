0782e97667f50b707d0d3823184fb05623b55239
Angelo <angelogoves@gmail.com>
2023-08-23 16:20:09 +0530

    updated exercise 6

--- RUBY/exercise-06/exercise-06.rb
+++ RUBY/exercise-06/exercise-06.rb
@@ -1,17 +1,32 @@
-require 'base64'
+require "base64"
 
 class Exercise
-  def base64_encode
+  def write_content
     content = "Originally, John Doe was a sham name used to indicate any plaintiff in an action of ejectment (a legal action to regain property) in civil court [ “ the string that will be base encoded ”]. Richard Roe was the counterpart, to indicate the defendant. These fake names were used in delicate legal matters, a practice that was abolished in English law in 1852."
-    File.open("RUBY/exercise-06/content.txt", "w") do |file|
+    content_file_path = "RUBY/exercise-06/content.txt"
+    File.open(content_file_path, "w") do |file|
       file.write(content)
     end
+    return content, content_file_path
+  end
+
+  def base64_encode
     read_content_file = File.read("RUBY/exercise-06/content.txt")
-    encoding_string = "the string that will be base encoded"
-    encoded_string = Base64.encode64(encoding_string).chomp
-    encoded_data = read_content_file.gsub(encoding_string, encoded_string)
-    new_file = File.open("RUBY/exercise-06/Angelo.txt", "w")
-    new_file.write(encoded_data)
-    new_file.close()
+    string_to_encode = "the string that will be base encoded"
+    encoded_string = Base64.encode64(string_to_encode).chomp
+    @encoded_data = read_content_file.gsub(string_to_encode, encoded_string)
+    return string_to_encode, encoded_string
+  end
+
+  def write_encoded_content(new_file_path)
+    begin
+      # new_file_path = "RUBY/exercise-06/Angelo.txt"
+      File.open(new_file_path, "w") do |file|
+        file.write(@encoded_data)
+      end
+    rescue => exception
+      return "Error in path"
+    end
+    return @encoded_data
   end
 end
--- RUBY/exercise-06/testcase-06.rb
+++ RUBY/exercise-06/testcase-06.rb
@@ -1,16 +1,24 @@
-require 'test/unit'
-require_relative 'exercise-06'
+require "test/unit"
+require_relative "exercise-06"
 
 class TestBase64Encode < Test::Unit::TestCase
-    def test_make_exercise_object
-        object_exercise = Exercise.new
-        assert object_exercise.instance_of? Exercise
-    end
-    def test_base64_encode
-        object_exercise = Exercise.new
-        object_exercise.base64_encode
-        expected_output = File.read("RUBY/exercise-06/content.txt")
-        actual_output = File.read("RUBY/exercise-06/Angelo.txt")
-        assert_not_equal(actual_output,expected_output)
-    end
+  def test_make_exercise_object
+    object_exercise = Exercise.new
+    assert object_exercise.instance_of? Exercise
+  end
+
+  def test_write_content
+    object_exercise = Exercise.new
+    content, file_path = object_exercise.write_content
+    output = File.open(file_path,'r')
+    assert_equal(content, output)
+  end
+
+  def test_base64_encode
+    object_exercise = Exercise.new
+    object_exercise.base64_encode
+    expected_output = File.read("RUBY/exercise-06/content2.txt")
+    actual_output = File.read("RUBY/exercise-06/Angelo.txt")
+    assert_not_equal(actual_output, expected_output)
+  end
 end
