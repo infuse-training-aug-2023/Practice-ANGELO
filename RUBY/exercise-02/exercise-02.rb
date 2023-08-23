a90d70174c827332a10e1b05242a491283fa5dae
Angelo <angelogoves@gmail.com>
2023-08-23 12:55:07 +0530

    checking if array index is out of bound

--- RUBY/exercise-02/exercise-02.rb
+++ RUBY/exercise-02/exercise-02.rb
@@ -1,14 +1,33 @@
 class Exercise
-    def element_at(arr,index)
-        return arr[index]
+  def element_at(arr, index)
+    if index <= arr.length
+      return arr[index]
+    else
+      return "Array index is out of bound"
     end
-    def inclusive_range(arr, start_pos, end_pos)
-        return arr[start_pos..end_pos]
+  end
+
+  def inclusive_range(arr, start_pos, end_pos)
+    if (start_pos < arr.length) && (end_pos <= arr.length) && (start_pos < end_pos)
+      return arr[start_pos..end_pos]
+    else
+      return "Array index is out of bound"
     end
-    def non_inclusive_range(arr, start_pos, end_pos)
-        return arr[start_pos...end_pos]
+  end
+
+  def non_inclusive_range(arr, start_pos, end_pos)
+    if (start_pos < arr.length) && (end_pos <= arr.length) && (start_pos < end_pos)
+      return arr[start_pos...end_pos]
+    else
+      return "Array index is out of bound"
     end
-    def start_and_length(arr, start_pos, lengths)
-        return arr[start_pos..lengths].length
+  end
+
+  def start_and_length(arr, start_pos, lengths)
+    if start_pos <= arr.length
+      return arr[start_pos..lengths].length
+    else
+      return "Array index is out of bound"
     end
+  end
 end
