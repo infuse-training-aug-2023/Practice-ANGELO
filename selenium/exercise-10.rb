c7d2c9f619a6e03dd0d98bd169ca0abb94c1c334
Angelo <angelogoves@gmail.com>
2023-08-23 17:06:37 +0530

    Applied suggested changes

--- selenium/exercise-10.rb
+++ selenium/exercise-10.rb
@@ -4,10 +4,10 @@ driver = Selenium::WebDriver.for :edge
 
 driver.get "https://computer-database.gatling.io/computers"
 
-table = driver.find_element(:tag_name, "tbody")
-table_column = table.find_elements(:tag_name, "a")
+table = driver.find_element(:xpath, '//table[starts-with(.,"Computer nameIntroducedDiscontinuedCompanyACE---AN/FSQ-3201")]')
+table_rows = table.find_elements(:xpath, ".//tbody/tr")
 
-table_column.each do |value|
-  puts value.text
+table_rows.each do |value|
+  puts value.find_elements(:tag_name, "td")[1].text
 end
 driver.quit
