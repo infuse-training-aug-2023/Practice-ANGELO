7845deed8ead3effdbc05c397934d8fb6bf8ebd4
Angelo <angelogoves@gmail.com>
2023-08-23 15:09:15 +0530

    Made a single function to do the task

--- RUBY/exercise-03/exercise-03.rb
+++ RUBY/exercise-03/exercise-03.rb
@@ -1,28 +1,20 @@
 class Exercise
-    def initialize(hash_values)
-        @hash_values = hash_values
+  def initialize(hash_values)
+    @hash_values = hash_values
+  end
+
+  def check_hash
+    store_integer_hash = Hash.new
+    @hash_values.each do |key, value|
+      if key.is_a?(Integer)
+        store_integer_hash[key] = value
+      end
     end
-    def hash_values=(hash_values)
-        @hash_values = hash_values
+    for key, value in store_integer_hash.to_a
+      if key % 2 == 0
+        @hash_values.delete(key)
+      end
     end
-    def hash_values
-        return @hash_values
-    end
-    def is_key_integer
-        store_op_keys = []
-        hash_values.each_key do |key|
-            if key.is_a?(Integer)
-                store_op_keys << key
-            end
-        end
-        return store_op_keys
-    end
-    def delete_even_keys
-        for key, value in hash_values.to_a
-            if key.is_a?(Integer) && key%2==0
-                hash_values.delete(key)
-            end
-        end
-        return hash_values
-    end
+    return [store_integer_hash, @hash_values]
+  end
 end
--- RUBY/exercise-03/testcase-03.rb
+++ RUBY/exercise-03/testcase-03.rb
@@ -1,43 +1,18 @@
-require 'test/unit'
-require_relative 'exercise-03'
+require "test/unit"
+require_relative "exercise-03"
 
 class TestCheckHash < Test::Unit::TestCase
-    @@hash_values = {
-        1 => 10,
-        2 => 20,
-        3 => 30,
-        4 => 10,
-        5 => 20,
-        "6" => 30,
-        7 => 10,
-        8 => 20,
-        "nine" => 30,
-        543121 => 100
-    }
-    def test_make_exercise_object
-        object_exercise = Exercise.new(@@hash_values)
-        assert object_exercise.instance_of? Exercise
-    end
-    def test_is_key_integer
-        object_exercise = Exercise.new(@@hash_values)
-        actual_output = [1, 2, 3, 4, 5, 7, 8, 543121]
-        assert_equal(actual_output, object_exercise.is_key_integer)
-    end
-    def test_delete_even_keys
-        @hash_values = {
-            1 => 10,
-            2 => 20,
-            3 => 30,
-            4 => 10,
-            5 => 20,
-            "6" => 30,
-            7 => 10,
-            8 => 20,
-            "nine" => 30,
-            543121 => 100
-        }
-        object_exercise = Exercise.new(@hash_values)
-        actual_output = {1=>10, 3=>30, 5=>20, "6"=>30, 7=>10, "nine"=>30, 543121=>100}
-        assert_equal(actual_output, object_exercise.delete_even_keys)
-    end
+  @@hash_values = { 1 => 10, 2 => 20, 3 => 30, 4 => 10, 5 => 20, "6" => 30, 7 => 10, 8 => 20, "nine" => 30, 543121 => 100 }
+
+  def test_make_exercise_object
+    object_exercise = Exercise.new(@@hash_values)
+    assert object_exercise.instance_of? Exercise
+  end
+
+  def test_check_hash
+    object_exercise = Exercise.new(@@hash_values)
+    actual_output = [{ 1 => 10, 2 => 20, 3 => 30, 4 => 10, 5 => 20, 7 => 10, 8 => 20, 543121 => 100 },
+                     { 1 => 10, 3 => 30, 5 => 20, "6" => 30, 7 => 10, "nine" => 30, 543121 => 100 }]
+    assert_equal(actual_output, object_exercise.check_hash)
+  end
 end
