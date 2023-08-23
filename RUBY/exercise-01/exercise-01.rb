04b3713caa44236be8f30dac944f3e22ffbcdd1d
Angelo <angelogoves@gmail.com>
2023-08-23 12:48:29 +0530

    checking for invalid input

--- RUBY/exercise-01/exercise-01.rb
+++ RUBY/exercise-01/exercise-01.rb
@@ -1,8 +1,14 @@
 class Exercise
-    def average(serial_average)
-        values = serial_average.split('-')
-        average_values = format('%.2f', (values[1].to_f + values[2].to_f) / 2)
-        result =  values[0] + "-" + average_values.to_s
+  def average(serial_average)
+    values = serial_average.split("-")
+    if values.length == 3
+      regex = /\d{3}-\d{2}.\d{2}-\d{2}.\d{2}/
+      if serial_average.match?(regex)
+        average_values = format("%.2f", (values[1].to_f + values[2].to_f) / 2)
+        result = values[0] + "-" + average_values.to_s
         return result
+      end
     end
+    return "Invalid Input"
+  end
 end
--- RUBY/exercise-01/testcase-01.rb
+++ RUBY/exercise-01/testcase-01.rb
@@ -1,15 +1,24 @@
-require 'test/unit'
-require_relative 'exercise-01'
+require "test/unit"
+require_relative "exercise-01"
 
 class TestAverage < Test::Unit::TestCase
-    def test_make_exercise_object
-        object_exercise = Exercise.new()
-        assert object_exercise.instance_of? Exercise
-    end
-    def test_check_average
-        object_exercise = Exercise.new()
-        assert_equal("002-15.00", object_exercise.average("002-10:00-20:00"))
-        assert_equal("002-50.00", object_exercise.average("002-100:00-00"))
-        assert_equal("002-50.00", object_exercise.average("002-100:00-one"))
-    end
+  def test_make_exercise_object
+    object_exercise = Exercise.new()
+    assert object_exercise.instance_of? Exercise
+  end
+
+  def test_check_average
+    object_exercise = Exercise.new()
+    assert_equal("002-15.00", object_exercise.average("002-10.00-20.00"))
+    assert_equal("002-20.00", object_exercise.average("002-40.00-00.00"))
+    assert_equal("003-50.00", object_exercise.average("003-10.00-90.00"))
+  end
+
+  def test_invalid_input
+    object_exercise = Exercise.new()
+    assert_equal("Invalid Input", object_exercise.average("002-10.00-10"))
+    assert_equal("Invalid Input", object_exercise.average("002-10-10.00"))
+    assert_equal("Invalid Input", object_exercise.average("00-10.00-10.00"))
+    assert_equal("Invalid Input", object_exercise.average("one-10.00-10"))
+  end
 end
