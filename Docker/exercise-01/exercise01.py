14b61941c5abe943a88bd875f97091cd8bc63c12
Angelo <angelogoves@gmail.com>
2023-08-24 23:27:05 +0530

    Tested command line input with sys.argv

--- Docker/exercise-01/exercise01.py
+++ Docker/exercise-01/exercise01.py
@@ -1,14 +1,20 @@
+import sys
+
 import numpy as np
 from PIL import Image
 
-array1 = np.arange(9)
+array1 = np.array(list(map(lambda i: int(sys.argv[i]), range(1, len(sys.argv)))))
 print("The original array: ", array1)
 array2 = array1.reshape(3, 3)
 print("The modified array: \n", array2)
 
-image = Image.open("dog.jpg")
-rgb_image = np.array(image)
-grey_image = np.dot(rgb_image[..., :3], [0.2989, 0.5870, 0.1140])
-grey_image = Image.fromarray(grey_image)
-grey_image = grey_image.convert("L")
-grey_image.save("dog_in_grey.jpg")
+try:
+    image = Image.open("dog.jpg")
+    rgb_image = np.array(image)
+    grey_image = np.dot(rgb_image[..., :3], [0.2989, 0.5870, 0.1140])
+    grey_image = Image.fromarray(grey_image)
+    grey_image = grey_image.convert("L")
+    grey_image.save("dog_in_grey.jpg")
+    print("Converted RGB image to Greyscale image")
+except Exception as error:
+    print("Error:", error)
