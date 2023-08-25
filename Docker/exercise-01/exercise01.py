06b7f5e62c87986097b8551cf10e098ec9e7ba53
Angelo <angelogoves@gmail.com>
2023-08-25 10:48:37 +0530

    used env variable

--- Docker/exercise-01/exercise01.py
+++ Docker/exercise-01/exercise01.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 import numpy as np
@@ -8,13 +9,14 @@ print("The original array: ", array1)
 array2 = array1.reshape(3, 3)
 print("The modified array: \n", array2)
 
-try:
-    image = Image.open("dog.jpg")
-    rgb_image = np.array(image)
-    grey_image = np.dot(rgb_image[..., :3], [0.2989, 0.5870, 0.1140])
-    grey_image = Image.fromarray(grey_image)
-    grey_image = grey_image.convert("L")
-    grey_image.save("dog_in_grey.jpg")
-    print("Converted RGB image to Greyscale image")
-except Exception as error:
-    print("Error:", error)
+if os.environ.get('EXEC'):
+    try:
+        image = Image.open("dog.jpg")
+        rgb_image = np.array(image)
+        grey_image = np.dot(rgb_image[..., :3], [0.2989, 0.5870, 0.1140])
+        grey_image = Image.fromarray(grey_image)
+        grey_image = grey_image.convert("L")
+        grey_image.save("dog_in_grey.jpg")
+        print("Converted RGB image to Greyscale image")
+    except Exception as error:
+        print("Error:", error)
